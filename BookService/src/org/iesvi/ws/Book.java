de5c2fedc5ad5a2f629576a0db1c0035f2aa0b87
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-11-10 00:12:47 +0100

    Corrections to the definition and implementation of the WebService and its methods.

--- BookService/src/org/iesvi/ws/Book.java
+++ BookService/src/org/iesvi/ws/Book.java
@@ -5,53 +5,49 @@ import javax.jws.WebService;
 import java.util.List;
 
 /**
- * WebService is a technology that uses a set of protocols and standards used to exchange data between applications.
- *
  * Book is the interface implemented by BookImpl where the methods that the class will use are defined.
+ * The WebService annotation is used here to specify that whatever class implements this interface
+ * will implement the code needed to create a WebService we can call.
  */
 @WebService
 public interface Book {
-
     /**
-     * WebMethod is an enterprise-wide business process integration suite built on java.
+     * This method lists the books currently stored.
+     * The WebMethod annotation is used here to specify that this method will be exposed
+     * as a callable function of the WebService using the defined parameters with the specified result.
      *
-     * This method list a list of books.
-     *
-     * @return a list of books
+     * @return a list defined as List<BookModel>
      */
     @WebMethod
     public List<BookModel> getBookList();
 
     /**
-     * WebMethod is an enterprise-wide business process integration suite built on java.
-     *
      * This method add a book into the list of books entered as a parameter.
+     * The WebMethod annotation is used here to specify that this method will be exposed
+     * as a callable function of the WebService using the defined parameters with the specified result.
      *
-     * @param bookList book list where we add the new book
      * @param newBook the new book that we add to the list
      */
     @WebMethod
-    public void addBook(List<BookModel> bookList, BookModel newBook);
+    public void addBook(BookModel newBook);
 
     /**
-     * WebMethod is an enterprise-wide business process integration suite built on java.
-     *
      * This method removes a book from the book list based on its position.
+     * The WebMethod annotation is used here to specify that this method will be exposed
+     * as a callable function of the WebService using the defined parameters with the specified result.
      *
-     * @param bookList book list where we add the new book
      * @param position the position where the book is
      */
     @WebMethod
-    public void deleteBookByPosition(List<BookModel> bookList, int position);
+    public void deleteBookByPosition(int position);
 
     /**
-     * WebMethod is an enterprise-wide business process integration suite built on java.
-     *
      * This method removes a book from the book list based on its identification number.
+     * The WebMethod annotation is used here to specify that this method will be exposed
+     * as a callable function of the WebService using the defined parameters with the specified result.
      *
-     * @param bookList book list where we add the new book
      * @param id the book identification number
      */
     @WebMethod
-    public void deleteBookById(List<BookModel> bookList, int id);
+    public void deleteBookById(int id);
 }
