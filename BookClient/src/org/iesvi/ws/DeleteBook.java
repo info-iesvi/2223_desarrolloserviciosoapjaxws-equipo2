ee03ece93da40fb99075a07066ae55020be2e81b
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-11-09 22:48:53 +0100

    BookClient: añadidas las clases

---
+++ BookClient/src/org/iesvi/ws/DeleteBook.java
@@ -0,0 +1,59 @@
+package org.iesvi.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para deleteBook complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="deleteBook">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="arg0" type="{http://ws.iesvi.org/}bookImpl" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "deleteBook", propOrder = {
+    "arg0"
+})
+public class DeleteBook {
+
+    protected BookImpl arg0;
+
+    /**
+     * Obtiene el valor de la propiedad arg0.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BookImpl }
+     *     
+     */
+    public BookImpl getArg0() {
+        return arg0;
+    }
+
+    /**
+     * Define el valor de la propiedad arg0.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BookImpl }
+     *     
+     */
+    public void setArg0(BookImpl value) {
+        this.arg0 = value;
+    }
+
+}
